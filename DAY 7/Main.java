39920bd686bd670a067122ec9873183db17faad6
sakethBainagari <saketh7727@gmail.com>
2024-11-17 21:07:11 +0530

    Day 7

---
+++ DAY 7/Main.java
@@ -0,0 +1,45 @@
+//https://hive.smartinterviews.in/contests/smart-interviews-primary/problems/checkerboard-pattern
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+
+        Scanner s = new Scanner(System.in);
+        int tc  = s.nextInt();
+        for(int t=0;t<tc;t++){
+            int n = s.nextInt();
+            System.out.println("Case #" + (t+1) + ":");
+            int a = 0;
+            int flag = 1;
+            for(int i=1;i<=n*2;i++){
+                if(a==0){
+                    flag =2;
+                }else if(a==1){
+                    flag = 1;
+                }
+
+                for(int j=1;j<=n;j++){
+                    if(flag==2){
+                        System.out.print("**");
+                        flag =1;
+                    }else if(flag == 1){
+                        System.out.print("--");
+                        flag = 2;
+                    }
+                }
+                System.out.println();
+                if(i%2 == 0){
+                    if(a==0){
+                        a=1;
+                    }else{
+                        a=0;
+                    }
+                }
+            }
+        }
+    }
+}

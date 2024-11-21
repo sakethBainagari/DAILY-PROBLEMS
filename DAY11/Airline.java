2d7d343b4c3956736dbf3005085298dfa5845a8b
sakethBainagari <saketh7727@gmail.com>
2024-11-21 22:24:58 +0530

    Day 11

--- DAY11/Airline.java
+++ DAY11/Airline.java
@@ -1,48 +1,27 @@
-// https://www.codechef.com/learn/course/arrays/ARRAYS02/problems/DSAAGP12?tab=statement
 
-import java.util.*;
+// https://www.codechef.com/problems/AIRLINE
+
 import java.lang.*;
-import java.io.*;
+import java.util.*;
+
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-        Scanner s = new Scanner(System.in);
-        int tc = s.nextInt();
-        while(tc-->0){
-            int n = s.nextInt();
-            int[] a = new int[n];
-            for(int i=0;i<n;i++){
-                a[i] = s.nextInt();
-            }
-            int maxSum = Integer.MIN_VALUE;
-            for(int i=0;i<n;i++){
-                int currSum = 0;
-                for(int j=i;j<n;j++){
-                    currSum += a[j];
-                    maxSum = Math.max(maxSum, currSum);
-                }
-            }
-            System.out.println(maxSum);
-        }
+		Scanner s = new Scanner(System.in);
+		int tc = s.nextInt();
+		while(tc-->0){
+		    int a = s.nextInt();
+		    int b = s.nextInt();
+		    int c = s.nextInt();
+		    int d = s.nextInt();
+		    int e = s.nextInt();
+		    if((a+b)<=d && c<=e  || (a+c)<=d && b<=e || (b+c)<=d && a<=e) {
+		        System.out.println("Yes");
+		    }else{
+		        System.out.println("No");
+		    }
+		}
 	}
 }
-
-
-//==================USING KADANE'S ALGO =====================
-
-private static int kadane(int[] nums) {
-        int currentSum = 0;
-        int maxSum = Integer.MIN_VALUE;
-        for (int num : nums) {
-            currentSum += num;
-            if (currentSum < num) {
-                currentSum = num;
-            }
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-            }
-        }
-        return maxSum;
-    }

5a6679451e0e50b84ea52fb6437e93a023d3b9c1
sakethBainagari <saketh7727@gmail.com>
2024-11-27 21:14:21 +0530

    Day 18

---
+++ DAY 18/x1sANDy0s.java
@@ -0,0 +1,35 @@
+
+//https://hive.smartinterviews.in/contests/smart-interviews-primary/problems/x-1s-and-y-0s?page=2&pageSize=10
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    static long power(long x, long y){
+        long a=x, res =1;
+        int mod = 1000000007;
+        while(y!=0){
+            if((y&1)==1){
+                res = res*a;
+                res %=mod;
+            }
+            a = (a*a)%mod;
+            y >>=1;
+
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+
+        Scanner s = new Scanner(System.in);
+        int tc = s.nextInt();
+        int mod = 1000000007;
+        while(tc-->0){
+            long x = s.nextLong();
+            long y = s.nextLong();
+            long ans = ((((long)power(2,x)%mod)-1)%mod) * ((long)power(2,y)%mod);
+            System.out.println(ans%mod);
+        }
+    }
+}
+

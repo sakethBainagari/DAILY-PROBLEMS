4de814df1e4a9b92a93a0a666db26c4972eec113
sakethBainagari <saketh7727@gmail.com>
2024-12-01 22:07:52 +0530

    Day 21

--- DAY 21 01-11-2024/leetcodeContestQuestion1.java
+++ DAY 21 01-11-2024/leetcodeContestQuestion1.java
@@ -7,7 +7,7 @@ class Solution {
         // for(int i=0;i<c.length;i++){
         //     if(c[i] == '0'){
         //         c[i] = '1';
-        //     }   
+        //     }                        
         // }
         // String bS = new String(c);
         // return Integer.parseInt(bS,2); 
@@ -16,7 +16,6 @@ class Solution {
         while (temp <= n) {
             temp = (temp << 1);    
         }
-        System.out.println(temp-1);
         return temp-1;
 
     }

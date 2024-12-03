febe1f931b31b8d2f3bceecf192473e0f6735535
sakethBainagari <saketh7727@gmail.com>
2024-12-03 21:49:58 +0530

    Day 23

---
+++ DAY 23 03-12-2024/removeele.java
@@ -0,0 +1,33 @@
+
+//https://leetcode.com/problems/remove-element/?envType=problem-list-v2&envId=array&difficulty=EASY
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != val){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+//OPTIMAL SOLUTION
+// two pointer for optimal solution
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0; 
+        int right = nums.length - 1; 
+
+        while (left <= right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+}

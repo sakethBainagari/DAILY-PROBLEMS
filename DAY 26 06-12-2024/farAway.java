442fcc9860adfcc6463bfb48a74057d7ae471f63
sakethBainagari <saketh7727@gmail.com>
2024-12-06 09:41:57 +0530

    Day 26

---
+++ DAY 26 06-12-2024/farAway.java
@@ -0,0 +1,31 @@
+//https://www.codechef.com/problems/FARAWAY?tab=statement
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int tc = s.nextInt();
+        while(tc-->0){
+            int n = s.nextInt();
+            long m = s.nextInt();
+            int[] a = new int[n];
+            long dist = 0;
+            for(int i=0;i<n;i++){
+                a[i] = s.nextInt();
+                if(a[i] <= m/2){
+                    dist +=Math.abs(m-a[i]);
+                }else if(a[i] > m/2){
+                    dist += Math.abs(a[i] - 1);
+                }
+            }
+            System.out.println(dist);
+            
+        }
+	}
+}

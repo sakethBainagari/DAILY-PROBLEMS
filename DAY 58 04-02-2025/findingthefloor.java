39f7904ed66a5e760fabcfb98a8a8f3aac5866bf
sakethBainagari <saketh7727@gmail.com>
2025-02-04 19:56:42 +0530

    Day 58

---
+++ DAY 58 04-02-2025/findingthefloor.java
@@ -0,0 +1,26 @@
+// https://hive.smartinterviews.in/contests/smart-interviews-primary/problems/finding-the-floor
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import bisect
+
+INT_MIN = -2147483648
+
+def find_floor(arr,x):
+    pos = bisect.bisect_right(arr,x)
+    if pos == 0:
+        return INT_MIN
+    else:
+        return arr[pos-1]
+
+def solve():
+    n = int(input())
+    arr = list(map(int,input().split()))
+    arr.sort()
+
+    q=int(input())
+    for i in range(q):
+        x = int(input())
+        print(find_floor(arr,x))
+
+solve()

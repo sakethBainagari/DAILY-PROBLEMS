71063c4ebbfcba8ce50a79d030cca360a53d55f8
sakethBainagari <saketh7727@gmail.com>
2025-02-04 19:57:46 +0530

    Day 58

---
+++ DAY 58 04-02-2025/findingFreq.java
@@ -0,0 +1,28 @@
+//https://hive.smartinterviews.in/contests/smart-interviews-primary/problems/finding-frequency?page=6&pageSize=10
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner s = new Scanner(System.in);
+        int n  = s.nextInt();
+        long[] a = new long[n];
+        Map<Long, Integer> freq = new HashMap<>();
+        for(int i=0;i<n;i++){
+            a[i] = s.nextLong();
+            freq.put(a[i], freq.getOrDefault(a[i],0)+1);  
+        }
+        int nofq = s.nextInt();
+        for(int i=0;i<nofq;i++){
+            long q = s.nextLong();
+            if(freq.containsKey(q)){
+                System.out.println(freq.get(q)) ;
+            }else{
+                System.out.println("0");
+            }
+        }
+
+    }
+}

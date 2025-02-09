d8a87d9d9cecc1ed3fd690b31452e7ad9d0478e0
sakethBainagari <saketh7727@gmail.com>
2025-02-09 21:15:46 +0530

    Day 59

---
+++ DAY 59 09-02-2025/pretynum.java
@@ -0,0 +1,27 @@
+
+//https://www.codechef.com/problems/NUM239?tab=statement
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while(t-->0){
+            int l = s.nextInt();
+            int r = s.nextInt();
+            int count = 0;
+            for(int i=l;i<=r;i++){
+                int n = i%10;
+                if(n==2 || n==3||n==9){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+	}
+}

e43c1e233cb476270d93b2679528c7ae2631b27a
sakethBainagari <saketh7727@gmail.com>
2025-02-12 21:22:43 +0530

    Day 61

---
+++ DAY 61 12-02-2025/moodyChef.java
@@ -0,0 +1,39 @@
+
+//https://www.codechef.com/problems/MOOCHEF?tab=statement
+
+import java.lang.*;
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int tc=s.nextInt();
+        while(tc-->0){
+            int n = s.nextInt();
+            int l = s.nextInt();
+            int r = s.nextInt();
+            int happi = 0;
+            int mini = 0;
+            int maxi = 0;
+            
+            for(int i=0;i<n;i++){
+                int num = s.nextInt();
+                if(num>=l && num<=r){
+                    happi++;
+                }else{
+                    happi--;
+                }
+                
+                mini = Math.min(mini,happi);
+                maxi = Math.max(maxi,happi);
+            }
+            System.out.println(maxi + " "+ mini);
+            
+        }
+        
+      
+	}
+}

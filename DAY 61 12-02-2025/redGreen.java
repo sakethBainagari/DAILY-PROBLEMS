29d2212c77a932478f46578e9544ab4b140e7c49
sakethBainagari <saketh7727@gmail.com>
2025-02-12 21:21:35 +0530

    Day 61

---
+++ DAY 61 12-02-2025/redGreen.java
@@ -0,0 +1,31 @@
+
+//https://www.codechef.com/problems/DOLL
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int tc = s.nextInt();
+        while(tc-->0){
+            int n = s.nextInt();
+            int h = s.nextInt();
+            int[] a = new int[n];
+            for(int i=0;i<n;i++){
+                a[i] = s.nextInt();
+            }
+            int count = 0;
+            for(int i=0;i<n;i++){
+                if(a[i] > h){
+                    count++;
+                    
+                }
+            }
+            System.out.println(count);
+        }
+	}
+}

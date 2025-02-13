c4cd7c9b3d9ca2f1a6e7c268e2b00b56cb55ea80
sakethBainagari <saketh7727@gmail.com>
2025-02-13 19:54:52 +0530

    Day 62

---
+++ DAY 62 13-02-2025/thatisMyScore.java
@@ -0,0 +1,31 @@
+
+//https://www.codechef.com/problems/WATSCORE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int tc  = sc.nextInt();
+        while(tc-->0){
+            int n = sc.nextInt();
+            int[] b = new int[9];
+            for(int i=0;i<n;i++){
+                int p = sc.nextInt();
+                int s = sc.nextInt();
+                if(p>=1 && p<=8){
+                    b[p] = Math.max(b[p],s);
+                }
+            }
+            int count=0;
+            for(int i=1;i<=8;i++){
+                count+=b[i];
+            }
+            System.out.println(count);
+        }
+	}
+}

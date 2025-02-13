6d59c538083d18afc05de07240dda6302dca60d6
sakethBainagari <saketh7727@gmail.com>
2025-02-13 19:53:20 +0530

    Day 62

---
+++ DAY 62 13-02-2025/chefDolls.java
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/MISSP
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int tc = s.nextInt();
+        while(tc-->0){
+            int n = s.nextInt();
+            int count=0;
+            for(int i=0;i<n;i++){
+                count ^= s.nextInt();
+            }
+            System.out.println(count);
+        }
+	}
+}

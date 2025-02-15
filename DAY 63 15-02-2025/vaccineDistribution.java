256fd944029c6ef85d834fd46b3fd9f0d8252952
sakethBainagari <saketh7727@gmail.com>
2025-02-15 19:28:00 +0530

    Day 63

---
+++ DAY 63 15-02-2025/vaccineDistribution.java
@@ -0,0 +1,34 @@
+
+//https://www.codechef.com/problems/VACCINE2?tab=statement
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int tc = s.nextInt();
+        while(tc-->0){
+            int n = s.nextInt();
+            int d = s.nextInt();
+            int[] a = new int[n];
+            for(int i=0;i<n;i++){
+                a[i] = s.nextInt();
+            }
+            int count1 =0;
+            int count2 =0;
+            for(int i =0 ;i<n;i++){
+                if(a[i] >=80 || a[i] <= 9){
+                    count1++;
+                }else{
+                    count2++;
+                }
+            }
+            int count = (int)Math.ceil((double)count1/d) + (int)Math.ceil((double)count2/d);
+            System.out.println((int)count);
+        }
+	}
+}

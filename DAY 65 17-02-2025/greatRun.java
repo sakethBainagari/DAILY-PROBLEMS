e2fab7c7c0e595f10cf7e798db6f7c5918f12e57
sakethBainagari <saketh7727@gmail.com>
2025-02-17 20:29:29 +0530

    Day 65

---
+++ DAY 65 17-02-2025/greatRun.java
@@ -0,0 +1,40 @@
+
+//https://www.codechef.com/problems/PROC18A?tab=ide
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int tc  = sc.nextInt();
+        while(tc-->0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] a = new int[n];
+            for(int i=0;i<n;i++){
+                a[i] = sc.nextInt();
+            }
+            int max =0;
+            int sum = 0;
+            for(int i =0;i<k;i++){
+                sum += a[i];
+                
+            }
+            max = sum;
+            
+            for(int i=k;i<n;i++){
+                sum = sum -a[i-k] + a[i];
+                max = Math.max(max,sum);
+            }
+            
+            
+            
+            
+            System.out.println(max);
+        }
+	}
+}

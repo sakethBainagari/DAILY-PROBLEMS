e8b89ec9740650cff618cf4f7a03aa930e1eaec9
sakethBainagari <saketh7727@gmail.com>
2025-02-19 05:54:54 +0530

    Day 67

---
+++ DAY 67 19-02-2025/mulofFive.java
@@ -0,0 +1,23 @@
+
+
+import java.lang.*;
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int tc = s.nextInt();
+        while(tc-->0){
+            int n = s.nextInt();
+            String ss = s.next();
+            if(ss.contains("0") || ss.contains("5")){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+	}
+}

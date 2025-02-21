6ad5565707287a85da2eba773d01ec9362d755d9
sakethBainagari <saketh7727@gmail.com>
2025-02-21 20:51:25 +0530

    Day 67

---
+++ DAY 67 19-02-2025/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = s.nextInt();
+
+        while (t-- > 0) {
+            String s = scanner.next();
+            int count = 0;
+            boolean isHapp = false;
+            for(char c: s.toCharArray()){
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                    count++;
+                    if(count > 2){
+                        isHapp = true;
+                        break;
+                    }
+                }else{
+                    count = 0;
+                }
+            }
+            
+            if(isHapp){
+                System.out.println("Happy");
+            }else{
+                System.out.println("Sad");
+            }
+        }
+    }
+}

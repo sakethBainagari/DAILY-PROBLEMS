08281ad232d778037ab31acc9b170475c346b085
sakethBainagari <saketh7727@gmail.com>
2025-02-22 19:46:19 +0530

    Day 67

---
+++ DAY 67 19-02-2025/ZOOZ.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int tc = s.nextInt();
+        while(tc-->0){
+            int n = s.nextInt();
+            for(int i=0;i<n;i++){
+                if(i==0||i==n-1){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+	}
+}

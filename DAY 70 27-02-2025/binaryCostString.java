0b802488ed65fa48dc1a77886ea8bc2f9d03180d
sakethBainagari <saketh7727@gmail.com>
2025-02-27 19:09:03 +0530

    Day 70

---
+++ DAY 70 27-02-2025/binaryCostString.java
@@ -0,0 +1,28 @@
+	//https://www.codechef.com/problems/BSCOST?tab=statement
+	import java.util.*;
+	import java.lang.*;
+	import java.io.*;
+	
+	class Codechef
+	{
+		public static void main (String[] args) throws java.lang.Exception
+		{
+			// your code goes here
+	        Scanner s = new Scanner(System.in);
+	        int tc = s.nextInt();
+	        while(tc-->0){
+	            int n = s.nextInt();
+	            int x = s.nextInt();
+	            int y = s.nextInt();
+	            
+	            String ss = s.next();
+	           if(ss.contains("01") || ss.contains("10")){
+	               System.out.println(Math.min(x,y));
+	           }else{
+	               System.out.println(0);
+	           }
+	            
+	        }
+		}
+	}
+	

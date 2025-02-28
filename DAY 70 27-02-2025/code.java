eca50dfe10b699233a5c5800575d9f861f8ff9ce
sakethBainagari <saketh7727@gmail.com>
2025-02-28 19:47:03 +0530

    Day 70

---
+++ DAY 70 27-02-2025/code.java
@@ -0,0 +1,31 @@
+//https://www.codechef.com/problems/TTENIS?tab=statement
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        while(n-->0){
+            String ss = s.next();
+            int count = 0;
+            int count0 = 0;
+            for(char ch : ss.toCharArray()){
+                if(ch == '1'){
+                    count++;
+                }else{
+                    count0++;
+                }
+            }
+            if(count > count0){
+                System.out.println("WIN");
+            }else{
+                System.out.println("LOSE");
+            }
+        }
+	}
+}

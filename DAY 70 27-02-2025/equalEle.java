2e4c299695f2023ae02666808551286eeca17656
sakethBainagari <saketh7727@gmail.com>
2025-03-01 06:20:08 +0530

    Day 70

---
+++ DAY 70 27-02-2025/equalEle.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s = new Scanner(System.in);
+        int tc = s.nextInt();
+        while(tc-->0){
+            int n = s.nextInt();
+            int[] a = new int[n];
+            Map<Integer,Integer> map = new HashMap<>();
+            for(int i=0;i<n;i++){
+                a[i] = s.nextInt();
+                map.put(a[i],map.getOrDefault(a[i],0)+1);
+            }
+            int max = 0;
+            for(int freq:map.values()){
+                if(freq> max){
+                    max = freq;
+                }
+            }
+            System.out.println(n-max);
+        }
+	}
+}

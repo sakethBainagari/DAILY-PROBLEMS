3717e88d54804feaa8e08d609105ef8ae12f7029
sakethBainagari <saketh7727@gmail.com>
2025-03-03 20:09:30 +0530

    Day 72

---
+++ DAY 72 03-03-2025/num.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/number-of-different-integers-in-a-string/description/
+class Solution {
+    public int numDifferentIntegers(String word) {
+        String[] words = word.replaceAll("[^0-9]"," ").split("\\s+");
+        Set<String> set = new HashSet<>();
+        for(String i : words){
+            if(!i.isEmpty()){
+               // set.add(String.valueOf(Integer.parseInt(i)));
+               // for large number use bellow one
+               set.add(String.valueOf(new java.math.BigInteger(i)));
+            }
+        }
+        return set.size();
+    }
+}

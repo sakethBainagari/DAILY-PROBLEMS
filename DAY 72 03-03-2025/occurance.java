2abd4398e0c7eb40ba5cd0412dd5c7ef7feefbbc
sakethBainagari <saketh7727@gmail.com>
2025-03-03 20:06:20 +0530

    Day 72

---
+++ DAY 72 03-03-2025/occurance.java
@@ -0,0 +1,17 @@
+
+//https://www.interviewbit.com/problems/occurence-of-each-number/
+public class Solution {
+    public ArrayList<Integer> findOccurences(ArrayList<Integer> A) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : A){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        TreeMap<Integer,Integer> sorted = new TreeMap<>(map);
+        ArrayList<Integer> res = new ArrayList<>();
+        for(Map.Entry<Integer,Integer> entry: sorted.entrySet()){
+            res.add(entry.getValue());
+        }   
+        return res;
+        
+    }
+}

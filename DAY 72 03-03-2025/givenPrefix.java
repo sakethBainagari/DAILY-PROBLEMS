47d612dc30277f976a1856f9963f8240151525d3
sakethBainagari <saketh7727@gmail.com>
2025-03-03 20:09:08 +0530

    Day 72

---
+++ DAY 72 03-03-2025/givenPrefix.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/counting-words-with-a-given-prefix/description/?envType=problem-list-v2&envId=array
+
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        for(int i=0;i<words.length;i++){
+            if(words[i].startsWith(pref)){
+                count++;
+            }
+        }
+        return count;
+    }
+}

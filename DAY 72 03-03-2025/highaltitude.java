1bfd3dd6cdc198fb44e757f037a1769b24001a5c
sakethBainagari <saketh7727@gmail.com>
2025-03-04 21:55:17 +0530

    Day 72

--- DAY 72 03-03-2025/highaltitude.java
+++ DAY 72 03-03-2025/highaltitude.java
@@ -1,8 +1,12 @@
 
 //https://leetcode.com/problems/find-the-highest-altitude/?envType=problem-list-v2&envId=array
+
 class Solution {
     public int largestAltitude(int[] gain) {
         int n = gain.length;
+
+
+        
         int max = 0;
         int curr = 0;
         for(int i=0;i<n;i++){

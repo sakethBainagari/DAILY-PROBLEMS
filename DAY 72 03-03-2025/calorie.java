b55a7e7edf5a501dc9ac92fb9a0ed2875678588a
sakethBainagari <saketh7727@gmail.com>
2025-03-04 21:55:57 +0530

    Day 72

---
+++ DAY 72 03-03-2025/calorie.java
@@ -0,0 +1,28 @@
+
+import java.lang.*;
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s  = new Scanner(System.in);
+		int tc  = s.nextInt();
+		while(tc-->0){
+		    int n = s.nextInt();
+		    int k = s.nextInt();
+		    int sum = 0;
+		    int count = 0;
+		    for(int i=0;i<n;i++){
+		        int a = s.nextInt();
+		        sum +=a;
+		        if(sum<=k){
+		            count++;
+		        }
+		    }
+		 
+		    System.out.println(count);
+		}
+
+	}
+}

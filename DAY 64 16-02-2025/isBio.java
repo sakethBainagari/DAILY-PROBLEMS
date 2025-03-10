05bda021603fd884aeb30a7fb690c9f63756fc90
sakethBainagari <saketh7727@gmail.com>
2025-03-10 20:50:22 +0530

    Day 64

--- DAY 64 16-02-2025/isBio.java
+++ DAY 64 16-02-2025/isBio.java
@@ -2,7 +2,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class isBio {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */

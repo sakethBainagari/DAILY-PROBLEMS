a78a6d3a2dafb24ba9ab51255e952d4cd71eec39
sakethBainagari <saketh7727@gmail.com>
2025-03-12 21:47:28 +0530

    Day 60

--- DAY 60 10-02-2025/gauntels.java
+++ DAY 60 10-02-2025/gauntels.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class gauntels {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */

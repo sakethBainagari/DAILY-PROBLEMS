e553df270320c4b14ed5325a4ac88994384f7443
sakethBainagari <saketh7727@gmail.com>
2025-03-13 20:46:21 +0530

    Day 58

--- DAY 58 04-02-2025/findingFreq.java
+++ DAY 58 04-02-2025/findingFreq.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class findingFreq {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
         Scanner s = new Scanner(System.in);

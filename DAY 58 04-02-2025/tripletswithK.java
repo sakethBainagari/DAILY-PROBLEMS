bce71b42c33d4e0447932db88bb968febfab4578
sakethBainagari <saketh7727@gmail.com>
2025-03-13 20:47:17 +0530

    Day 58

--- DAY 58 04-02-2025/tripletswithK.java
+++ DAY 58 04-02-2025/tripletswithK.java
@@ -2,7 +2,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class tripletswithK {
     public static boolean findtriplet(int[] arr, int n,int k){
         for(int i=0;i<n-2;i++){
             int left = i+1,right =n-1;
